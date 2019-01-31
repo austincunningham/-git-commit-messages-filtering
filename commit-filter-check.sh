dd991cd8df8a8184fc8befc18bee904a315f6675
Austin Cunningham <aucunnin@redhat.com>
2019-01-31 15:58:54 +0000

    test2

--- commit-filter-check.sh
+++ commit-filter-check.sh
@@ -4,4 +4,4 @@ currentbranch=`git branch | grep \* | cut -d ' ' -f2`
 echo $currentbranch 
 git log $currentbranch --pretty=format:"%H" --not master >> shafile.txt
 
-git log --format=%B -n 1 $1
+#git log --format=%B -n 1 $1
