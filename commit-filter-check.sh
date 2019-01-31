968b7ad113a4f793cca8a6dc362437f260a9937d
Austin Cunningham <aucunnin@redhat.com>
2019-01-31 15:53:54 +0000

    feat: first commit

---
+++ commit-filter-check.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+currentbranch=`git branch | grep \* | cut -d ' ' -f2`
+echo $currentbranch 
+git log $currentbranch --pretty=format:"%H" --not master > shafile.txt
+
+git log --format=%B -n 1 $1
