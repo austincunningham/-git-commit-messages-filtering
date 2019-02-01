5e9959f5b99f7918503a1aef1d37337c2914d11a
Austin Cunningham <aucunnin@redhat.com>
2019-02-01 14:42:36 +0000

    feat:(commit-file-filter): testing edge cases (AEROGEAR-3423)

--- commit-filter-check.sh
+++ commit-filter-check.sh
@@ -2,20 +2,23 @@
 
 # Get the current branch and apply it to a variable
 currentbranch=`git branch | grep \* | cut -d ' ' -f2`
-#echo $currentbranch 
+
 # Gets the commits for the current branch and outputs to file
 git log $currentbranch --pretty=format:"%H" --not master > shafile.txt
 
 # loops through the file an gets the message
 for i in `cat ./shafile.txt`;
 do 
-  #echo $i
-  gitmessage=`git log --format=%B -n 1 "$i"`;
+  # gets the git commit message based on the sha
+  gitmessage=`git log --format=%B -n 1 "$i"`
+
   # fix(commit-filter-check): add commit messages (AEROGEAR-038928990423)
   messagecheck=`echo $gitmessage | grep '[( )\-\:]'`
+  # check to see if the messagecheck var is empty
   if [ -z "$messagecheck"]
   then 
         echo "failed commit message check"
+        rm shafile.txt
         return false
   else
         echo "commit message passed"
