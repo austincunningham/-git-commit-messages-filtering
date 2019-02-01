c930039428c4b32ff0e83b27edfe914446ef5626
Austin Cunningham <aucunnin@redhat.com>
2019-02-01 08:53:31 +0000

    fix(commit-filter-check): add commit messages (AEROGEAR-SomeNo)

--- commit-filter-check.sh
+++ commit-filter-check.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-rm shafile.txt
-currentbranch=`git branch | grep \* | cut -d ' ' -f2`
-echo $currentbranch 
-git log $currentbranch --pretty=format:"%H" --not master >> shafile.txt
 
-#git log --format=%B -n 1 $1
+# Get the current branch and apply it to a variable
+currentbranch=`git branch | grep \* | cut -d ' ' -f2`
+#echo $currentbranch 
+# Gets the commits for the current branch and outputs to file
+git log $currentbranch --pretty=format:"%H" --not master > shafile.txt
+
+# loops through the file an gets the message
+for i in `cat ./shafile.txt`;
+do 
+  #echo $i
+  gitmessage=`git log --format=%B -n 1 "$i"`;
+  echo $gitmessage
+  
+done
+rm shafile.txt
